f3f57da8d8af73d746f9d35bd92992063ee5da5d
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-04-10 00:23:29 +0800

    使用@babel/parser 分析content

--- bundler.js
+++ bundler.js
@@ -1,9 +1,13 @@
 // 没有webpack,用bundler.js做打包
 const fs = require('fs')
+const parser = require('@babel/parser')
+
 
 const moduleAnalyser = (filename) => {
     const content = fs.readFileSync(filename,'utf-8')
-    console.log(content)
+    console.log(parser.parse(content,{
+        sourceType: 'module'
+    }))
 }
 
 moduleAnalyser('./src/index.js')
