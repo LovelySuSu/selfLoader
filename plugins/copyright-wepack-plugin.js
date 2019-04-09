b90a1cdfc8b6ef8a2ae4c599e84ca6b72146b7a7
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-04-09 23:48:15 +0800

    compiler

--- plugins/copyright-wepack-plugin.js
+++ plugins/copyright-wepack-plugin.js
@@ -1,10 +1,16 @@
-class CoprrightWebpackPlugin {
+class CopyrightWebpackPlugin {
     constructor(options) {
         console.log(options)
     }
+    // compiler是webpack的实例
     apply(compiler) {
-
+        compiler.hooks.emit.tapAsync('CopyrightWebpackPlugin',(compilation,cb) => {
+            //compilation 本次打包的内容
+            console.log('打包完成')
+            console.log(compilation.assets)
+            cb()
+        })
     }
 }
 
-module.exports = CoprrightWebpackPlugin
+module.exports = CopyrightWebpackPlugin
