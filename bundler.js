387239e03cc2160abbdfa9b55111d21af56b862e
dingding <dingding@xudinghanyangdeMacBook-Pro.local>
2019-04-10 00:25:13 +0800

    分析语法树

--- bundler.js
+++ bundler.js
@@ -5,9 +5,10 @@ const parser = require('@babel/parser')
 
 const moduleAnalyser = (filename) => {
     const content = fs.readFileSync(filename,'utf-8')
-    console.log(parser.parse(content,{
+    const ast = parser.parse(content,{
         sourceType: 'module'
-    }))
+    })
+    console.log(ast.program.body)
 }
 
 moduleAnalyser('./src/index.js')
